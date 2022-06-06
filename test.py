79bef9f63e04592b15e0f59f6f2c2124b7ecec54
Jeremy Howard <j@fast.ai>
2022-06-06 16:49:19 +1000

    nb

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ from execnb import *
 s = CaptureShell()
 cmds = ["""import matplotlib.pyplot as plt
 plt.figure(figsize=(1,1))
-plt.plot([1,2])""", "print('1')\n2", """from nbdev.showdoc import show_doc
-show_doc(bool)""", "raise Exception"]
+plt.plot([1,2])""",
+    "print('1')\n2",
+    "from nbdev.showdoc import show_doc\nshow_doc(bool)",
+    "raise Exception"
+]
 
-#cmds = """#print('1')
-#2"""
 for cmd in cmds: print(s.run(cmd),'\n')
+assert s.result==2
 
